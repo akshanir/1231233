b94ce1b378143c10b29106e258b4fb32ca500123
Ammar Shanir <71748382+akshanir@users.noreply.github.com>
2022-11-05 12:50:26 +0200

    Add files via upload

---
+++ 2112022/plaything.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+
+import os
+import time
+import json
+import glob
+
+import matplotlib.pyplot as plt
+from PIL import Image
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import seaborn as sns
+
+tf.__version__
+model = tf.keras.models.load_model('my_model.h5')
+
+# Check its architecture
+model.summary()
+
+def format_y(y):
+    return ''.join(map(lambda x: chr(int(x)), y))
+
+def predict(image_path):
+    im = Image.open(image_path).resize((120, 100))
+    # im = im.resize((H, W))
+    im = np.array(im) / 255.0
+    im = np.array(im)
+    
+    y_pred = model.predict(np.array([im]))
+    y_pred = tf.math.argmax(y_pred, axis=-1)
+    
+    print('predict: %s' % format_y(y_pred[0]))
+    return format_y(y_pred[0])
+    # plt.imshow(im)
+
+
+print(predict('out.jfif'))
